13e06bc46617ed9fad00baa451be2f52d4942db2
SidZhoop <siddharth@zhoop.in>
2023-08-02 22:56:00 +0530

    setup

---
+++ src/pages/index.js
@@ -0,0 +1,19 @@
+import About from "./About";
+import Auth from "./Auth";
+import Companies from "./Companies";
+import CompanyProfile from "./CompanyProfile";
+import FindJobs from "./FindJobs";
+import JobDetail from "./JobDetail";
+import UploadJob from "./UploadJob";
+import UserProfile from "./UserProfile";
+
+export {
+  FindJobs,
+  Auth,
+  Companies,
+  UserProfile,
+  CompanyProfile,
+  UploadJob,
+  About,
+  JobDetail,
+};
