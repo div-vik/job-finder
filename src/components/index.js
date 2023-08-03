82d63a4f11b7cc116a7ebe0225d9b55d067ecdba
SidZhoop <siddharth@zhoop.in>
2023-08-03 16:25:31 +0530

    text input added

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import CustomButton from "./CustomButton";
+import TextInput from "./TextInput";
 
-export { Navbar, Footer, CustomButton };
+export { Navbar, Footer, CustomButton, TextInput };
