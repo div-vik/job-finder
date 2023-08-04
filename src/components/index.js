cdf036ffc907446a0352f5868c17bb7721c4e8bc
SidZhoop <siddharth@zhoop.in>
2023-08-04 17:17:39 +0530

    header, list box and job card components added

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,17 @@ import Navbar from "./Navbar";
 import CustomButton from "./CustomButton";
 import TextInput from "./TextInput";
 import SignUp from "./SignUp";
+import Header from "./Header";
+import ListBox from "./ListBox";
+import JobCard from "./JobCard";
 
-export { Navbar, Footer, CustomButton, TextInput, SignUp };
+export {
+  Navbar,
+  Footer,
+  CustomButton,
+  TextInput,
+  SignUp,
+  Header,
+  ListBox,
+  JobCard,
+};
