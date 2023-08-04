b6ab5966b575b9309613cdc88af756427b5bba4b
SidZhoop <siddharth@zhoop.in>
2023-08-04 13:08:33 +0530

    sign up component added

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Footer from "./Footer";
 import Navbar from "./Navbar";
 import CustomButton from "./CustomButton";
 import TextInput from "./TextInput";
+import SignUp from "./SignUp";
 
-export { Navbar, Footer, CustomButton, TextInput };
+export { Navbar, Footer, CustomButton, TextInput, SignUp };
